418f262866074acb226d653865315a829b0db276
Anthony Ng <newyork.anthonyng@gmail.com>
2016-09-09 06:43:49 -0400

    Add service worker to offline.html

---
+++ Responsive_Responsible_Images/app.js
@@ -0,0 +1,9 @@
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js')
+		.then(function(registration) {
+			console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		})
+		.catch(function(err) {
+			console.log('ServiceWorker registration failed: ', err);
+		});
+}
